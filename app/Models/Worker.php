72a08e3de129654f29d5130148b93577d5c9818b
cbkelvin <kelvinsnir@gmail.com>
2021-11-17 00:18:02 +0300

    snm

--- app/Models/Worker.php
+++ app/Models/Worker.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Worker extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $table = "Workers";
 
     protected $fillable = ["name", "email", "password", "phone_no"];
